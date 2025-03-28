fb4196e4e404b261d86227b88933d25cf3694b2e
Ahmed Abdelaziz <ahmed.abdelaziz.gm@gmail.com>
2025-03-28 12:16:55 +0200

    extend tmdb entries and update data

--- fetch_tmdb.py
+++ fetch_tmdb.py
@@ -1,44 +1,58 @@
 import os
-import csv
 import time
 import requests
+from utils import save_csv
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
-START = 1
-END = 250
 API_KEY = os.getenv("TMDB_API_KEY")
-BASE_URL = "https://api.themoviedb.org/3/discover/movie"
+FIELDNAMES = ["tmdbID", "Title", "Year"]
+TV_SHOWS_URL = "https://api.themoviedb.org/3/discover/tv"
+MOVIES_URL = "https://api.themoviedb.org/3/discover/movie"
 
 params = {
-    "page": START,
     "api_key": API_KEY,
+    "include_adult": "true",
     "with_origin_country": "EG",
-    "sort_by": "release_date.asc",
 }
 
-movies = []
 
-for page in range(START, END):
-    params["page"] = page
-    response = requests.get(BASE_URL, params=params)
-    data = response.json()
+def fetch_entries(url, title_key, year_key):
+    page = 1
+    movies = []
+    params["sort_by"] = f"{year_key}.asc"
 
-    for movie in data.get("results", []):
-        id = movie.get("id")
-        year = movie.get("release_date", "")[:4]
-        title = movie.get("original_title", "") or movie.get("title", "")
-        movies.append([id, title, year])
+    while True:
+        params["page"] = page
+        response = requests.get(url, params=params)
+        data = response.json()
+        results = data.get("results", [])
 
-    print(f"Fetched page {page}")
-    time.sleep(0.2)
+        for result in results:
+            id = result.get("id")
+            year = result.get(year_key, "")[:4]
+            title = result.get(f"original_{title_key}", "") or result.get(title_key, "")
+            movies.append(
+                {
+                    fieldname: value
+                    for fieldname, value in zip(FIELDNAMES, [id, title, year])
+                }
+            )
+
+        print(f"Fetched page {page}")
+        time.sleep(0.1)
+        if len(results) == 0:
+            break
+        page += 1
+
+    return movies
 
 
-with open(
-    f"tmdb_egypt_movies_{START}_{END}.csv", "w", newline="", encoding="utf-8"
-) as f:
-    writer = csv.writer(f)
-    writer.writerow(["tmdbID", "Title", "Year"])
-    writer.writerows(movies)
+if __name__ == "__main__":
+    movies = fetch_entries(MOVIES_URL, "title", "release_date")
+    tv_shows = fetch_entries(TV_SHOWS_URL, "name", "first_air_date")
+
+    save_csv(f"./data/tmdb-mix-1to{len(movies)}.csv", movies, FIELDNAMES)
+    save_csv(f"./data/tmdb-mix-1to{len(tv_shows) + 1}.csv", tv_shows, FIELDNAMES)
